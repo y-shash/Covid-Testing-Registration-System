1a4ead6b4b5a87f172d1dce5227fd951fd0676bc
Abdalla Mohamed Ihab Ahmed Mohamed Abdellatif <aabd0025@student.monash.edu>
2022-05-04 16:32:43 +0300

    Added the booking and the measurements classes to models file.

--- FIT3077_Covid_Booking_System/polls/models.py
+++ FIT3077_Covid_Booking_System/polls/models.py
@@ -12,3 +12,21 @@ class User(models.Model):
     isHealthcareWorker = models.BooleanField(default=False)
     bookings = []
     additionalInfo = {}
+
+
+class Booking(models.Model):
+    customerId = models.CharField(max_length=200)
+    testingSiteId = models.CharField(max_length=200)
+    startTime = models.DateTimeField()
+    notes = models.CharField(max_length=200)
+    additionalInfo = {}
+
+
+class Measurement(models.Model):
+    location = models.CharField(max_length=200)
+    destination = models.CharField(max_length=200)
+    distance = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Distance from {self.location} to {self.destination} is {self.distance} km"
