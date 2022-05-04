cade834cb8639ca83aa464ef8bea930fb252b32e
Abdalla Mohamed Ihab Ahmed Mohamed Abdellatif <aabd0025@student.monash.edu>
2022-05-04 16:47:27 +0300

    Created login class to save login details, and form class to save answers.

--- FIT3077_Covid_Booking_System/polls/views.py
+++ FIT3077_Covid_Booking_System/polls/views.py
@@ -79,3 +79,28 @@ class RAT(CovidTest, ABC):
 class PCR(CovidTest, ABC):
     def getTimeTaken(self):
         return self.type + " takes" + " 72 hours"
+
+class Login(object):
+    def __init__(self, username):
+        self.username = username
+        self.id = ""
+
+    def setId(self, id):
+        self.id = id
+
+    def getUserName(self):
+        return self.username
+
+
+class Form(object):
+    def __init__(self, q1, q2, q3, q4, q5, q6):
+        self.q1 = q1
+        self.q2 = q2
+        self.q3 = q3
+        self.q4 = q4
+        self.q5 = q5
+        self.q6 = q6
+
+    def getTotalAnswers(self):
+        return self.q1 + self.q2 + self.q3 + self.q4 + self.q5 + self.q6
+
